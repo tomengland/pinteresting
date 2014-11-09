033aa19b5198995f22fcead715a754c31efd2019
Thomas England <tengland808@gmail.com>
2014-11-08 22:14:11 -1000

    Devise setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Set this to Heroku hostname.
+  config.action_mailer.default_url_options = { :host => 'http://teio-pinteresting.herokuapp.com/' }
 end
