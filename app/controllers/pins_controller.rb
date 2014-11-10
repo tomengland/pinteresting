7d6b0bb283b0e621bcccc6be4a71998c6c01c157
Thomas England <tengland808@gmail.com>
2014-11-09 17:44:19 -1000

    Rubocop

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,3 +1,4 @@
+# PinsController for my pins
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
@@ -39,14 +40,17 @@ class PinsController < ApplicationController
   end
 
   private
-    def set_pin
-      @pin = Pin.find(params[:id])
-    end
-    def correct_user
-      @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authroized to edit this pin" if @pin.nil?
-    end
-    def pin_params
-      params.require(:pin).permit(:description)
-    end
+
+  def set_pin
+    @pin = Pin.find(params[:id])
+  end
+
+  def correct_user
+    @pin = current_user.pins.find_by(id: params[:id])
+    redirect_to pins_path, notice: 'Not authroized to edit this pin' if @pin.nil?
+  end
+
+  def pin_params
+    params.require(:pin).permit(:description)
+  end
 end
